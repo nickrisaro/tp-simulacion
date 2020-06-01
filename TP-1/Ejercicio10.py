7d5aa0cfaa6f90420e50b1e34184f1659838d532
Nick Risaro <nrisaro@gmail.com>
2020-06-01 20:10:56 -0300

    Agrega gráfico con la evolución de contagios respecto del tiempo

--- TP-1/Ejercicio10.py
+++ TP-1/Ejercicio10.py
@@ -7,15 +7,40 @@ import sys
 from matplotlib.animation import FuncAnimation
 import matplotlib.patches as mpatches
 
-LIMIT_X = 100
-LIMIT_Y = 100
-RW_CUATRO_DIRS_VEL = 1
+LIMITE_X = 100
+LIMITE_Y = 100
+VELOCIDAD = 1
 
 CANT_INSTANTES = 4000
 CANT_PERSONAS = 100
 PROPORCION_INICIAL_INFECTADAS = 0.05
 PROBABILIDAD_CONTAGIO = 0.6
 DISTANCIA_CONTAGIO = 2.0
+
+class Grafico_epidemia:
+    def __init__(self, axes):
+
+        self.personas_sanas = np.array([])
+        self.personas_infectadas = np.array([])
+        self.instantes = np.array([])
+        line_sanas, = axes.plot(self.instantes, self.personas_sanas, label="Sanas", color="green")
+        line_infectadas, = axes.plot(self.instantes, self.personas_infectadas, label="Infectadas", color="red")
+        line_sanas.axes.axis([0, CANT_INSTANTES, 0, CANT_PERSONAS])
+        self.grafico_linea_sanas = line_sanas
+        self.grafico_linea_infectadas = line_infectadas
+
+    def actualizar_grafico_linea(self, instante, sanas, infectadas):
+
+        red_patch = mpatches.Patch(color="red", label="Infectadas {0}".format(infectadas))
+        green_patch = mpatches.Patch(color="green", label="Sanas {0}".format(sanas))
+
+        self.personas_sanas = np.append(self.personas_sanas, sanas)
+        self.personas_infectadas = np.append(self.personas_infectadas, infectadas)
+        self.instantes = np.append(self.instantes, instante)
+        self.grafico_linea_sanas.set_data(self.instantes, self.personas_sanas)
+        self.grafico_linea_infectadas.set_data(self.instantes, self.personas_infectadas)
+        self.grafico_linea_sanas.axes.axis([0, CANT_INSTANTES, 0, CANT_PERSONAS])
+        self.grafico_linea_sanas.axes.legend(handles=[red_patch, green_patch], loc="lower right", title="Instante {0}".format(instante))
 
 class Persona:
 
@@ -25,18 +50,18 @@ class Persona:
         self.y = y
 
     def esta_en_limites(self, x,y):
-        return (0 <= x <= LIMIT_X and 0 <= y <= LIMIT_Y)
+        return (0 <= x <= LIMITE_X and 0 <= y <= LIMITE_Y)
 
     def desplazarse(self):
         siguientePaso = random.randint(1,4)
         if siguientePaso == 1:
-            new_x,new_y = self.x + RW_CUATRO_DIRS_VEL, self.y
+            new_x,new_y = self.x + VELOCIDAD, self.y
         elif siguientePaso == 2:
-            new_x,new_y = self.x, self.y + RW_CUATRO_DIRS_VEL
+            new_x,new_y = self.x, self.y + VELOCIDAD
         elif siguientePaso == 3:
-            new_x,new_y = self.x - RW_CUATRO_DIRS_VEL, self.y
+            new_x,new_y = self.x - VELOCIDAD, self.y
         else:
-            new_x,new_y = self.x, self.y - RW_CUATRO_DIRS_VEL
+            new_x,new_y = self.x, self.y - VELOCIDAD
 
         if self.esta_en_limites(new_x,new_y):
             self.x, self.y = new_x, new_y
@@ -54,8 +79,7 @@ class Persona:
                     self.sana = np.random.uniform(0, 1) <= PROBABILIDAD_CONTAGIO
                     break
 
-
-def animate_random_walk(instante, personas, scat_personas, ax):
+def animate_random_walk(instante, personas, scat_personas, grafico_epidemia):
     """Esta funcion se ejecuta en cada frame del random walk y hace que se muevan y, posiblemente, se contagien las personas"""
 
     personas_x = np.array([])
@@ -82,9 +106,8 @@ def animate_random_walk(instante, personas, scat_personas, ax):
     coordenadas = np.column_stack((personas_x,personas_y))
     scat_personas.set_offsets(coordenadas)
     scat_personas.set_color(colors)
-    red_patch = mpatches.Patch(color="red", label="Infectadas {0}".format(infectadas))
-    green_patch = mpatches.Patch(color="green", label="Sanas {0}".format(sanas))
-    ax.legend(handles=[red_patch, green_patch], loc="lower left", title="Instante {0}".format(instante))
+
+    grafico_epidemia.actualizar_grafico_linea(instante, sanas, infectadas)
 
 def main():
 
@@ -93,8 +116,8 @@ def main():
     personas_y = np.array([])
     colors = np.array([])
     for i in range(0, CANT_PERSONAS):
-        x = np.random.uniform(0, LIMIT_X)
-        y = np.random.uniform(0, LIMIT_Y)
+        x = np.random.uniform(0, LIMITE_X)
+        y = np.random.uniform(0, LIMITE_Y)
 
         sana = np.random.uniform(0, 1) >= PROPORCION_INICIAL_INFECTADAS
 
@@ -107,11 +130,14 @@ def main():
         else:
             colors = np.append(colors, "r")
 
-    fig = plt.figure()
-    ax = plt.axes(xlim=(0, LIMIT_X), ylim=(0, LIMIT_Y))
-    scat_personas = ax.scatter(personas_x, personas_y, s=100, c=colors)
+    fig, axs = plt.subplots(2)
+    axs[0].set_xlim(0, LIMITE_X)
+    axs[0].set_ylim(0, LIMITE_Y)
+    scat_personas = axs[0].scatter(personas_x, personas_y, s=100, c=colors)
 
-    anim = FuncAnimation(fig, animate_random_walk, frames=CANT_INSTANTES, interval=1, repeat=False, fargs=(personas,scat_personas, ax))
+    grafico_epidemia = Grafico_epidemia(axs[1])
+
+    anim = FuncAnimation(fig, animate_random_walk, frames=CANT_INSTANTES, interval=1, repeat=False, fargs=(personas,scat_personas, grafico_epidemia))
     plt.show()
 
 if __name__ == "__main__":
