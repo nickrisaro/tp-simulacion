0ab709825012e912c52691bef69cfa4ea0419416
Martin Andújar <mandujar@fi.uba.ar>
2020-06-30 15:42:56 -0300

    Fix logic with some improvements

--- TP-2/Ejercicio2.py
+++ TP-2/Ejercicio2.py
@@ -1,5 +1,6 @@
 import random as rn
 import sys
+import numpy as np
 import matplotlib.pyplot as plt
 
 # Cola M/M/1
@@ -11,32 +12,54 @@ def simular(ms):
 	estados = []
 	cantidadEnCola = 0
 	cantidadVecesInactiva = 0
-	PROBABILIDAD_NUEVA_SOLICITUD = p*(1-q)
-	PROBABILIDAD_DE_TERMINAR = q*(1-p)
+	solicitudEnProceso = False
 	# tiempo = 0
 	for tiempo in range(ms):
-		if (rn.random() <= PROBABILIDAD_NUEVA_SOLICITUD):
-			cantidadEnCola += 1
-		if (rn.random() <= PROBABILIDAD_DE_TERMINAR and cantidadEnCola > 0):
-			cantidadEnCola -=1
+		if solicitudEnProceso and rn.random() <= q:
+			solicitudEnProceso = False
+			if (cantidadEnCola > 0):
+				cantidadEnCola -=1
+				solicitudEnProceso = True
+
+		if (rn.random() <= p):
+			if solicitudEnProceso: 
+				cantidadEnCola += 1
+			else:
+				solicitudEnProceso = True
+
 		tiempos.append(tiempo)
 		estados.append(cantidadEnCola)
-		if (cantidadEnCola == 0):
+		if (not solicitudEnProceso):
 			cantidadVecesInactiva+=1
 
 	return tiempos, estados, cantidadVecesInactiva
 
 
 tiempo = 100000
-print("c. Realice un gráfico mostrando la cantidad de solicitudes en el servidor en cada instante de tiempo.")
+print("b. Realice un gráfico mostrando la cantidad de solicitudes en el servidor en cada instante de tiempo.")
 tiempos, estados, cantidadVecesInactiva = simular(tiempo)
 plt.plot(tiempos, estados)
 plt.savefig(sys.path[0] + "/E02-cantidad-solicitudes-por-instante.png")
 
-print("d. Realice un histograma mostrando cuantas veces el sistema estuvo en cada estado")
+print("c. Realice un histograma mostrando cuantas veces el sistema estuvo en cada estado")
 plt.figure(figsize=(15,9))
 plt.hist(estados)
 plt.savefig(sys.path[0] + "/E02-cantidad-solicitudes-histograma.png")
+plt.clf()
+plt.cla()
+plt.close()
 
 print("d. Determine el % de tiempo que el servidor se encuentra sin procesar solicitudes")
-print(cantidadVecesInactiva / len(estados) * 100 , "%")
+
+# Realizo 1000 simulaciones
+cantidad_simu = 1000
+cantidades_inactivas = []
+rango_simulaciones = range(cantidad_simu) 
+for x in rango_simulaciones:
+	tiempos, estados, cantidadVecesInactiva = simular(tiempo)
+	cantidades_inactivas.append(cantidadVecesInactiva)
+
+plt.figure(figsize=(15,9))
+plt.plot( rango_simulaciones, cantidades_inactivas)
+plt.savefig(sys.path[0] + "/E02-cantidad-veces-inactivas-por-instante.png") 
+print(sum(cantidades_inactivas) / ( tiempo * cantidad_simu ) * 100 , "%")
