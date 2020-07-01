d51b75cceb47c5a374b2729dce6db3e6581ce02e
Martin Andújar <mandujar@fi.uba.ar>
2020-07-01 16:11:04 -0300

    Add labels to axes

--- TP-2/Ejercicio2.py
+++ TP-2/Ejercicio2.py
@@ -36,20 +36,28 @@ def simular(ms):
 
 
 tiempo = 100000
-print("b. Realice un gráfico mostrando la cantidad de solicitudes en el servidor en cada instante de tiempo.")
+print("c. Realice un gráfico mostrando la cantidad de solicitudes en el servidor en cada instante de tiempo.")
 tiempos, estados, cantidadVecesInactiva = simular(tiempo)
 plt.plot(tiempos, estados)
+plt.xlabel('Instante')
+plt.ylabel('Cantidad de Solicitudes en el servidor')
 plt.savefig(sys.path[0] + "/E02-cantidad-solicitudes-por-instante.png")
+plt.clf()
+plt.cla()
+plt.close()
 
-print("c. Realice un histograma mostrando cuantas veces el sistema estuvo en cada estado")
+print("d. Realice un histograma mostrando cuantas veces el sistema estuvo en cada estado")
 plt.figure(figsize=(15,9))
 plt.hist(estados)
+plt.xlabel('Cantidad de Solicitudes en el servidor')
+plt.ylabel('Instante')
+plt.ticklabel_format(style='plain')
 plt.savefig(sys.path[0] + "/E02-cantidad-solicitudes-histograma.png")
 plt.clf()
 plt.cla()
 plt.close()
 
-print("d. Determine el % de tiempo que el servidor se encuentra sin procesar solicitudes")
+print("e. Determine el % de tiempo que el servidor se encuentra sin procesar solicitudes")
 
 # Realizo 1000 simulaciones
 cantidad_simu = 1000
@@ -59,10 +67,10 @@ for x in rango_simulaciones:
 	tiempos, estados, cantidadVecesInactiva = simular(tiempo)
 	cantidades_inactivas.append(cantidadVecesInactiva)
 
-print (max(cantidades_inactivas))
-print (min(cantidades_inactivas))
 plt.figure(figsize=(15,9))
 plt.plot( rango_simulaciones, cantidades_inactivas)
+plt.xlabel('Número de simulación')
+plt.ylabel('Cantidad de veces con sistema inactivo')
 plt.savefig(sys.path[0] + "/E02-cantidad-veces-inactivas-por-instante.png") 
 print("Minimo veces inactivas", max(cantidades_inactivas))
 print("Maximo de veces inactivas", min(cantidades_inactivas) )
